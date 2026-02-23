ee421da411ebabeea46418d8bdb05924d68b76c2
CarroXR <bd563418626@gmail.com>
2026-02-23 12:46:07 +0900

    update p0485

--- app/src/main/java/com/carro/leetcode/solutions/p0485/Solution.java
+++ app/src/main/java/com/carro/leetcode/solutions/p0485/Solution.java
@@ -1,18 +1,20 @@
 package com.carro.leetcode.solutions.p0485;
 
 class Solution {
+
     public int findMaxConsecutiveOnes(int[] nums) {
-        int maxConsec = 0;
-        int count = 0;
+        int maxCount = 0;     // これまでの最大コンボ（ハイスコア）
+        int currentCount = 0; // 現在進行中のコンボ
+
         for (int num : nums) {
             if (num == 1) {
-                count++;
+                currentCount++; // 1ならコンボ継続
+                // 常にハイスコアを更新するかチェックする
+                maxCount = Math.max(maxCount, currentCount);
             } else {
-                maxConsec = Math.max(maxConsec, count);
-                count = 0;
+                currentCount = 0; // 0ならコンボリセット
             }
         }
-        maxConsec = Math.max(maxConsec, count);
-        return maxConsec;
+        return maxCount;
     }
 }
