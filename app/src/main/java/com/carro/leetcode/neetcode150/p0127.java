a0b17b88a53fe517662771c3d3f325eb16c9a89a
CarroXR <bd563418626@gmail.com>
2026-02-24 02:13:26 +0900

    restart the project with neetcode150, add p0127

---
+++ app/src/main/java/com/carro/leetcode/neetcode150/p0127.java
@@ -0,0 +1,17 @@
+package com.carro.leetcode.neetcode150;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class p0127 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> existed = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (existed.contains(nums[i])) {
+                return true;
+            }
+            existed.add(nums[i]);
+        }
+        return false;
+    }
+}
---
+++ app/src/test/java/com/carro/leetcode/neetcode150/p0127Test.java
@@ -0,0 +1,44 @@
+package com.carro.leetcode.neetcode150;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class p0127Test {
+    private final p0127 solution = new p0127();
+
+    @Test
+    void returnsTrueWhenArrayContainsDuplicate() {
+        boolean result = solution.containsDuplicate(new int[] {1, 2, 3, 1});
+
+        assertTrue(result);
+    }
+
+    @Test
+    void returnsFalseWhenArrayHasAllUniqueValues() {
+        boolean result = solution.containsDuplicate(new int[] {1, 2, 3, 4});
+
+        assertFalse(result);
+    }
+
+    @Test
+    void returnsTrueWhenArrayContainsNegativeDuplicate() {
+        boolean result = solution.containsDuplicate(new int[] {-1, -2, -3, -1});
+
+        assertTrue(result);
+    }
+
+    @Test
+    void returnsFalseForSingleElementArray() {
+        boolean result = solution.containsDuplicate(new int[] {42});
+
+        assertFalse(result);
+    }
+
+    @Test
+    void returnsFalseForEmptyArray() {
+        boolean result = solution.containsDuplicate(new int[] {});
+
+        assertFalse(result);
+    }
+}
